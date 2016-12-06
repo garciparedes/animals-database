0285d59c9070aa2df17bfae7ef6fa733450de88c
Sergio Garcia <sergio@garciparedes.me>
2016-12-06 12:16:00 +0100

    Worked on `propiedad` overlapping check

--- definitions/tables/propiedad.sql
+++ definitions/tables/propiedad.sql
@@ -19,8 +19,6 @@ CREATE TABLE propiedad (
     fin              TIMESTAMP NULL DEFAULT NULL,
 
 
-    UNIQUE (id_animal, id_responsable, inicio_propiedad),
-
     CONSTRAINT propiedad__clave
     PRIMARY KEY (id_animal, inicio_propiedad),
 
@@ -29,9 +27,14 @@ CREATE TABLE propiedad (
     FOREIGN KEY (id_responsable)
     REFERENCES responsable (id_responsable),
 
+
     CONSTRAINT propiedad__animal
     FOREIGN KEY (id_animal)
     REFERENCES animal (id_animal),
+
+
+    CONSTRAINT propiedad__unique
+    UNIQUE (id_animal, id_responsable, inicio_propiedad),
 
 
     CONSTRAINT propiedad__date
@@ -40,5 +43,28 @@ CREATE TABLE propiedad (
             THEN
                 inicio_propiedad <= fin
         END
+    ),
+
+
+    CONSTRAINT propiedad__overlapping
+    CHECK (
+        NOT exists(
+            SELECT *
+            FROM propiedad p
+            WHERE
+                p.id_animal = id_animal AND
+                (
+                    (
+                        p.inicio_propiedad >= inicio_propiedad AND
+                        p.fin <= inicio_propiedad
+                    ) OR
+                    (
+                        p.inicio_propiedad >= fin AND
+                        p.fin <= fin
+                    )
+                )
+
+        )
+
     )
 );
