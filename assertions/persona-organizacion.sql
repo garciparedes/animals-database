5c76f0446c1322ae619ecf7d735b0a4f105e5341
Sergio Garcia <sergio@garciparedes.me>
2016-12-07 22:59:29 +0100

    Changes in assertion and updated overleaf report commit

--- assertions/persona-organizacion.sql
+++ assertions/persona-organizacion.sql
@@ -1,4 +1,13 @@
 CREATE ASSERTION persona_organizacion_unique
-CHECK((SELECT COUNT(*)
-        FROM  persona p, organizacion o
-        WHERE p.id_responsable = o.id_responsable) = 0);
+CHECK(
+    (
+        SELECT COUNT(*)
+            FROM
+                responsable r
+                persona p,
+                organizacion o
+            WHERE
+                r.id_responsable = p.id_responsable OR
+                r.id_responsable = o.id_responsable
+    ) = 1
+);
